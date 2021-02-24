ea74baac34d1a1180377df9ae241071716225fde
Aiandrox <aiandrox7@gmail.com>
2021-02-24 21:55:29 +0900

    Add emoji-mart https://qiita.com/ozaki25/items/f8207cf4a2e7e4a95e89

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { Emoji } from "emoji-mart";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <Emoji emoji="thinking_face" size={64} />
+
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
