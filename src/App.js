afcd98c75ebcaa07df9593668eff5c92f9328472
Aiandrox <aiandrox7@gmail.com>
2021-02-25 00:31:13 +0900

    Add state

--- src/App.js
+++ src/App.js
@@ -1,17 +1,25 @@
+import { useState } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import { Emoji } from "emoji-mart";
 import { FormControl, Input } from "@material-ui/core";
 
 function App() {
+  const [typingString, setTypingString] = useState("");
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <Emoji emoji="thinking_face" size={64} />
 
+        {typingString}
         <FormControl>
-          <Input id="my-input" value={{ typingString }} />
+          <Input
+            id="my-input"
+            value={typingString}
+            onChange={(e) => setTypingString(e.target.value)}
+          />
         </FormControl>
 
         <a
