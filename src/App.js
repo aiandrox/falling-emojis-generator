16893c8a3fa4c92249a98d597fc5db8a4452c137
Aiandrox <aiandrox7@gmail.com>
2021-02-25 23:00:43 +0900

    Add search interval

--- src/App.js
+++ src/App.js
@@ -1,19 +1,31 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "./App.css";
 import { Emoji, NimbleEmojiIndex } from "emoji-mart";
 import data from "emoji-mart/data/google.json";
 import { FormControl, Input } from "@material-ui/core";
 
+let searchTimerId = null;
+
 function App() {
   const [typingString, setTypingString] = useState("");
   const [emoji, setEmoji] = useState("");
-  function search(value) {
-    setTypingString(value);
+
+  useEffect(() => {
+    if (searchTimerId !== null) {
+      clearTimeout(searchTimerId);
+    }
+    searchTimerId = setTimeout(() => {
+      search();
+    }, 500);
+  }, [typingString]);
+
+  function search() {
+    const value = typingString;
     if (value === "") return;
 
-    let emojiIndex = new NimbleEmojiIndex(data);
-    let emojiArray = emojiIndex.search(value);
-    let firstEmoji = emojiArray[0];
+    const emojiIndex = new NimbleEmojiIndex(data);
+    const emojiArray = emojiIndex.search(value);
+    const firstEmoji = emojiArray[0];
     if (!firstEmoji) return;
 
     if (value === firstEmoji["id"] || emojiArray.length === 1) {
@@ -31,7 +43,7 @@ function App() {
           <Input
             id="my-input"
             value={typingString}
-            onChange={(e) => search(e.target.value)}
+            onChange={(e) => setTypingString(e.target.value)}
           />
         </FormControl>
       </header>
